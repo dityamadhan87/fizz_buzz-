b4cbb736a85a05086b40d7216815ab581b893483
Raditya Rahmadhani <119946277+dityamadhan87@users.noreply.github.com>
2024-02-16 21:09:12 +0700

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,23 +1,23 @@
 class Solution:
     def fizzBuzz(self, n: int) -> list[str]:
-        hasil = []
+        result = []
         for i in range(1, n+1):
             if i % 3 == 0 and i % 5 == 0:
-                hasil.append("FizzBuzz")
+                result.append("FizzBuzz")
             elif i % 3 == 0:
-                hasil.append("Fizz")
+                result.append("Fizz")
             elif i % 5 == 0:
-                hasil.append("Buzz")
+                result.append("Buzz")
             else:
-                hasil.append(str(i))
-        return hasil
+                result.append(str(i))
+        return result
 
 n = int(input())
-hasil_list = Solution().fizzBuzz(n)
+list_result = Solution().fizzBuzz(n)
 list_to_string = "["
 index = 0
-for item in hasil_list:
-    if index == len(hasil_list) - 1:
+for item in list_result:
+    if index == len(list_result) - 1:
         list_to_string += '"' + str(item) + '"]'
         break
     list_to_string += '"' + str(item) + '",'
